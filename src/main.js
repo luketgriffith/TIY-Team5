a6973fd10e45c51fef804d7d450044647e53527a
John Rae <john.rae23@gmail.com>
2015-10-14 16:14:27 -0400

    Emptied function from main.js

--- src/main.js
+++ src/main.js
@@ -1,5 +1,19 @@
 (function () {
 
-  console.log('It Works!');
+  // script for news block in html
+  (function (){
+    //declare url for api data
+    let newsUrl = 'https://json-data.herokuapp.com/restaurant/news/1'
+
+
+
+
+  }());
+
+
+
+
+
+
 
 }());
