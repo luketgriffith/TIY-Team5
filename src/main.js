edd43d2d7ca4c435c3b2141a5d44293afa11ff55
John Rae <john.rae23@gmail.com>
2015-10-14 16:17:33 -0400

    Added news.js and removed it works function from main.js

--- src/main.js
+++ src/main.js
@@ -1,15 +1,5 @@
 (function () {
 
-  // script for news block in html
-  (function (){
-    //declare url for api data
-    let newsUrl = 'https://json-data.herokuapp.com/restaurant/news/1'
-
-
-
-
-  }());
-
 
 
 
