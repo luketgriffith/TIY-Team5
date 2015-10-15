6ffc0a64dca1bbafd20b00055640e3c2ec8ac2d6
Charlie Miller <charlie@thecharlah.com>
2015-10-15 10:20:14 -0400

    Finished menu js

--- src/charlie.js
+++ src/charlie.js
@@ -4,25 +4,57 @@
   let menuURL = 'https://json-data.herokuapp.com/restaurant/menu/3';
 
 // templates
-  let beerData = function (obj) {
-    return `
+
+  let beerData = function (obj) { 
+
+    $('.beer').append(`
     <div class="menuItemTitle">${ obj.item }</div>
     <div class="menuItemPrice">${ obj.price }</div>
-    <div class="menuItemDescription">${ obj.description }</div>`
+    <div class="menuItemDescripti;on">${ obj.description }</div>
+    `)
+
+    if (obj.allergies === 1) {
+      $('.beer').append("<div class='allergyContainer'>ALLERGY</div>");
+    };
+
+    if (obj.favorite === 1) {
+      $('.beer').append("<div class='favoriteContainer'>FAVORITE</div>");
+    };
+
+    $('.beer').append("<br>");
+
   };
 
   let entreesData = function (obj) {
-    return `
+    $('.entrees').append(`
     <div class="menuItemTitle">${ obj.item }</div>
     <div class="menuItemPrice">${ obj.price }</div>
-    <div class="menuItemDescription">${ obj.description }</div>`
+    <div class="menuItemDescripti;on">${ obj.description }</div>
+    `)
+
+    if (obj.allergies === 1) {
+      $('.entrees').append("<div class='allergyContainer'>ALLERGY</div>");
+    };
+
+    if (obj.favorite === 1) {
+      $('.entrees').append("<div class='favoriteContainer'>FAVORITE</div>");
+    };
+
+    $('.entrees').append("<br>");
   };
 
   let gamesData = function (obj) {
-    return `
+    $('.games').append(`
     <div class="menuItemTitle">${ obj.item }</div>
     <div class="menuItemPrice">${ obj.price }</div>
-    <div class="menuItemDescription">${ obj.description }</div>`
+    <div class="menuItemDescripti;on">${ obj.description }</div>
+    `)
+
+    if (obj.favorite === 1) {
+      $('.games').append("<div class='favoriteContainer'>FAVORITE</div>");
+    };
+
+    $('.games').append("<br>");
   };
 
 // function to put menu items on page
@@ -32,16 +64,16 @@
       method: 'get'
     }).then (function(data){
       _.each(data.Beer, function (item) {
-      var beerHTML = beerData(item);  
-      $('.beer').append(beerHTML);  
+        var beerHTML = beerData(item);  
+        $('.beer').append(beerHTML);  
       });
       _.each(data.entrees, function (item) {
-      var entreesHTML = entreesData(item);  
-      $('.entrees').append(entreesHTML);  
+        var entreesHTML = entreesData(item);  
+        $('.entrees').append(entreesHTML);  
       });
       _.each(data.games, function (item) {
-      var gamesHTML = gamesData(item);  
-      $('.games').append(gamesHTML);  
+        var gamesHTML = gamesData(item);  
+        $('.games').append(gamesHTML);  
       });
     });
 
