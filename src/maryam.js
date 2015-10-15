25b6319a0832bc6ee028fa42c8b929c43191db04
Maryam Ali <mali62541@gmail.com>
2015-10-15 12:22:24 -0400

    added slider bar

--- src/maryam.js
+++ src/maryam.js
@@ -1,4 +1,4 @@
-  
+ //for flickr pics on right hand side 
 function foodPics(setId) {
   
   var URL = 
@@ -21,6 +21,14 @@ function foodPics(setId) {
 }
 
 
+//for slider div
+
+(function slider(){
+  $('.sliderSetup').children('div').click(function(){
+    $('.main')removeClass('main');
+
+  })
+});
 
 
 
@@ -44,58 +52,3 @@ function foodPics(setId) {
 
 
 
-
-//Ignore. (notes below)
-
-// $(document).ready(function() {
-  
-  //fill in a set number here if needed
-
-//   foodPics('');
-// });
-
-
-  //let foodPicturesURL='https://api.flickr.com/services/rest       [fill in apiKEY here] '
-
-//"https://api.flickr.com/services/rest/" + 
-    
-    // "?method=flickr.photosets.getPhotos" +  
-    
-    //apiKey
-
-    // "&api_key={{315259924b1acacbb7bf64f20f866672}}" +  
-    
-    //take another look @the setID below
-    // "&photoset_id=" + 21277725634 +
-
-    //1 is for all public photos
-
-    // "&privacy_filter=1" +
-
-    //increase '20' if more pics are needed
-
-    // "&per_page=20" +
-
-    // "&format=json&nojsoncallback=1";
-
-//=============
-//   var apiKEY='';
-// }
-
-// $.getJSON('http://api.flickr.com/services/rest/'
-//     {
-//       method: 'flick.'
-//     }
-
-//   )
-
-
-// "http://api.flickr.com/services/rest/",
-//         {
-//             method: 'flickr.people.getPublicPhotos',
-//             api_key: apiKey,
-//             user_id: the_user_id,
-//             format: 'json',
-//             nojsoncallback: 1,
-//             per_page: 10 // you can increase this to get a bigger array
-//         },
